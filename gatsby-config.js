6e4d1d664de96428e23da9a31c0148ae1bb599ec
Vitor Dalbem Costa <dalbemcosta@gmail.com>
2024-01-31 15:18:05 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portifolio_vitor`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
